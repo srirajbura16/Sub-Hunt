f78a72081ab944faa31200b5c27aaea9da40f5b9
Sriraj Bura <sriraj.bura16@gmail.com>
2022-10-23 16:29:43 -0400

    modify getSubscriptions api and render channels

--- pages/api/getSubscriptions.js
+++ pages/api/getSubscriptions.js
@@ -1,4 +1,4 @@
-import { getToken } from "next-auth/jwt";
+import { getToken } from 'next-auth/jwt';
 
 const secret = process.env.NEXTAUTH_SECRET;
 
@@ -8,27 +8,36 @@ export default async function handler(req, res) {
   const data = await fetch(
     `https://youtube.googleapis.com/youtube/v3/subscriptions?part=snippet%2CcontentDetails&maxResults=50&mine=true&key=${process.env.GOOGLE_API}`,
     {
-      credentials: "include",
+      credentials: 'include',
       headers: {
         Authorization: `Bearer ${token.accessToken}`,
-        "Content-type": "application/json;charset=UTF-8",
+        'Content-type': 'application/json;charset=UTF-8',
       },
     }
   );
 
   const response = await data.json();
 
-  console.log(response);
+  const subscriptions = await Promise.all(
+    response.items.map(async (sub) => {
+      const channelId = sub.snippet.resourceId.channelId;
+      const { title, description } = sub.snippet;
 
-  let subscriptions = [];
-  //
-  response.items.map((sub) => {
-    subscriptions.push({
-      title: sub.snippet.title,
-      description: sub.snippet.description,
-      channelId: sub.snippet.resourceId.channelId,
-    });
-  });
+      const data = await fetch(
+        `https://yt.lemnoslife.com/channels?part=about&id=${channelId}`
+      );
+
+      const response = await data.json();
+
+      return {
+        title: title,
+        description: description,
+        links: response.items[0].about.links,
+      };
+    })
+  );
+
+  console.log(subscriptions, 'SHDJFSLKDFNLKDSNFLJNKSLKDNFLSKD');
 
   res.status(200).json(subscriptions);
 }
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,28 @@
-import { useSession, signIn, signOut } from "next-auth/react";
+import { useSession, signIn, signOut } from 'next-auth/react';
+import { useState } from 'react';
 
 export default function Component() {
+  const [subscriptions, setSubscriptions] = useState([]);
   const { data: session } = useSession();
+  let searchText;
 
   if (!session) {
     return (
       <>
         Not signed in <br />
-        <button onClick={() => signIn("google")}>Sign in with google</button>
+        <button onClick={() => signIn('google')}>Sign in with google</button>
       </>
     );
   }
+
+  const submit = async (e) => {
+    e.preventDefault();
+    searchText = e.target.searchText.value;
+    const response = await fetch('/api/getSubscriptions');
+    const subscriptions = await response.json();
+    console.log(subscriptions);
+    setSubscriptions(subscriptions);
+  };
 
   return (
     <>
@@ -20,20 +32,48 @@ export default function Component() {
       </div>
       <br />
 
-      <div class="mb-6">
-        <label
-          for="confirm_password"
-          class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+      <form onSubmit={submit}>
+        <div className="mb-6">
+          <input
+            type="text"
+            name="searchText"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="twitter"
+            required
+          />
+        </div>
+
+        <button
+          type="submit"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
         >
-          Confirm password
-        </label>
-        <input
-          type="text"
-          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          placeholder="twitter"
-          required
-        />
-      </div>
+          Search
+        </button>
+      </form>
+
+      {subscriptions ? (
+        <div>
+          {subscriptions.map((sub, i) => {
+            const { title, links } = sub;
+            return (
+              <div key={i}>
+                <div>{title}</div>
+                <ul>
+                  {links.map((link, ind) => {
+                    return <li key={ind}>{link.url}</li>;
+                  })}
+                </ul>
+              </div>
+            );
+          })}
+        </div>
+      ) : (
+        ''
+      )}
     </>
   );
 }
+// on submit
+// get inputtext from input and get subscriptions links
+// search inputtext in links and filter array
+// rendern based on input links
